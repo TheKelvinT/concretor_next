a90a75add587f25316fc7d5acf04402ff425cb6d
Kelvin Tan <tnchnkit@hotmail.com>
2024-08-21 10:32:10 +0800

    code clean up

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,35 +1,7 @@
-"use client";
-import React from "react";
-import EventCTA from "@/components/Events/EventCTA";
-import EventDesc from "@/components/Events/EventDesc";
-import EventSlider from "@/components/Events/EventSlider";
-import Gallery from "@/components/Events/Gallery";
-import Image from "next/image";
-import EventHero from "@/assets/eventhero.png";
-import { getEvents } from "@/sanity/sanity-utils";
+"use client"
 
-type Props = {};
+type Props = {}
 
 export default async function Events(props: Props) {
-  const EventData = await getEvents();
-  const data = EventData[0];
-
-  return (
-    <div>
-      <div className="-mt-[120px] min-w-screen ">
-        <Image
-          src={data.banner.image}
-          height="1000"
-          width="2000"
-          alt="hero-banner"
-          className="object-cover w-screen h-auto"
-        />
-      </div>
-
-      <EventCTA data={data} />
-      <EventDesc data={data} />
-      <EventSlider data={data} />
-      <Gallery />
-    </div>
-  );
+  return <div>Contact</div>
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,22 +1,8 @@
 "use client"
-import Image from "next/image"
 
-import MainCarousel from "@/components/MainCarousel"
-
-import ContactSection from "@/components/ContactSection"
-import {
-  getCardSection,
-  getCarousel,
-  getHomeContent,
-  getHomeNavigation,
-} from "@/sanity/sanity-utils"
-import { useEffect } from "react"
 import axios from "axios"
 
 export default async function Home() {
-  useEffect(() => {
-    getCharacter()
-  }, [])
   const getCharacter = async () => {
     const res = await axios.get("https://rickandmortyapi.com/api/character")
     console.log(res)
