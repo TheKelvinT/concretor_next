155a2745cfbd83b0e46b14b0e74e39d58e7bba51
Kelvin Tan <tnchnkit@hotmail.com>
2024-09-05 23:47:52 +0800

    navbar bug fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,52 +1,55 @@
-import React, { useState, useRef, useEffect } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useRouter } from "next/navigation";
-import Button from "@/components/Button";
+import React, { useState, useRef, useEffect } from "react"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import { useRouter } from "next/navigation"
+import Button from "@/components/Button"
 
-const leftNavigation = [{ name: "CONCRETOR", href: "/" }];
+const leftNavigation = [{ name: "CONCRETOR", href: "/" }]
 
 const rightNavigation = [
   { name: "About", href: "/about" },
   { name: "Services", href: "/#services" },
   { name: "Blog", href: "/blog" },
   { name: "Contact", href: "/contact" },
-];
+]
 
 function Navbar() {
-  const router = useRouter();
-  const [open, setOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
+  const router = useRouter()
+  const [open, setOpen] = useState(false)
+  const menuRef = useRef<HTMLDivElement>(null)
 
   const onClickContactUs = () => {
-    router.push("/contact");
-  };
+    router.push("/contact")
+  }
 
   useEffect(() => {
     const handler = (e: MouseEvent) => {
       if (menuRef.current && !menuRef.current.contains(e.target as Node)) {
-        setOpen(false);
+        setOpen(false)
       }
-    };
+    }
 
-    document.addEventListener("mousedown", handler);
+    document.addEventListener("mousedown", handler)
     return () => {
-      document.removeEventListener("mousedown", handler);
-    };
-  }, []);
+      document.removeEventListener("mousedown", handler)
+    }
+  }, [])
 
   const toggleMenu = () => {
-    setOpen(!open);
-  };
+    setOpen(!open)
+  }
 
   return (
     <div className="relative z-50 text-white py-4 bg-primary">
       <div className="mx-auto max-w-7xl px-4 md:px-16">
         <div className="relative flex items-center justify-between">
-          
           {/* Left Navigation for mobile */}
           <div className="flex items-center md:hidden">
             {leftNavigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-3xl font-merriweather">
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-3xl font-merriweather"
+              >
                 {item.name}
               </a>
             ))}
@@ -71,7 +74,11 @@ function Navbar() {
             <div className="flex-grow flex items-center justify-between">
               <div>
                 {leftNavigation.map((item) => (
-                  <a key={item.name} href={item.href} className="text-3xl font-merriweather">
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="text-3xl font-merriweather"
+                  >
                     {item.name}
                   </a>
                 ))}
@@ -82,7 +89,11 @@ function Navbar() {
                     {item.name}
                   </a>
                 ))}
-                <Button title={"Get Quote"} styleType="white" onClick={onClickContactUs} />
+                <Button
+                  title={"Get Quote"}
+                  styleType="white"
+                  onClick={onClickContactUs}
+                />
               </div>
             </div>
           </div>
@@ -92,7 +103,7 @@ function Navbar() {
         {open && (
           <div
             ref={menuRef}
-            className="absolute top-16 left-0 w-full bg-primary z-50 md:hidden bg-[#E6F3F3]"
+            className="absolute top-16 left-0 w-full  z-50 md:hidden bg-[#E6F3F3]"
           >
             <div className="text-center mt-16">
               {rightNavigation.map((item) => (
@@ -110,7 +121,7 @@ function Navbar() {
         )}
       </div>
     </div>
-  );
+  )
 }
 
-export default Navbar;
+export default Navbar
