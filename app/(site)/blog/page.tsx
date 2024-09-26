a846ede04f607e42c3fe1ec6656201a45596abec
Kelvin Tan <tnchnkit@hotmail.com>
2024-09-26 22:16:04 +0800

    blog set up

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,9 +1,17 @@
 "use client"
-import React from "react"
+import React, { useEffect } from "react"
 
-import { getBlogHero,getBlogs, getFeaturedBlogs } from "@/sanity/sanity-utils"
+import {
+  getBlogHero,
+  getBlogs,
+  getFeaturedBlogs,
+  fetchBlogPost,
+} from "@/sanity/sanity-utils"
 type Props = {}
 
 export default async function BlogPosts(props: Props) {
+  useEffect(() => {
+    fetchBlogPost()
+  }, [])
   return <div>blog</div>
 }
--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -1,12 +1,13 @@
-import { createClient, groq } from "next-sanity";
-import clientConfig from "./config/client-config";
-import { Blog, BlogHero } from "@/types/Blogs";
-import { Faq } from "@/types/Faq";
-import { Careers } from "@/types/Careers";
-import { Reservations } from "@/types/Reservations";
-import { Home } from "@/types/Home";
-import { Menu } from "@/types/Menu";
-import { Events } from "@/types/Events";
+import { createClient, groq } from "next-sanity"
+import clientConfig from "./config/client-config"
+import { Blog, BlogHero } from "@/types/Blogs"
+import { Faq } from "@/types/Faq"
+import { Careers } from "@/types/Careers"
+import { Reservations } from "@/types/Reservations"
+import { Home } from "@/types/Home"
+import { Menu } from "@/types/Menu"
+import { Events } from "@/types/Events"
+import axios from "axios"
 
 export async function getBlogs(): Promise<Blog[]> {
   return createClient(clientConfig).fetch(
@@ -19,7 +20,13 @@ export async function getBlogs(): Promise<Blog[]> {
         "image": image.asset -> url,
         content
     }`
-  );
+  )
+}
+export const fetchBlogPost = async () => {
+  const response = await axios.get(
+    " https://say9s8oc.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22blog%22%5D%7B%0A%20%20%20%20%20%20_id%2C%0A%20%20%20%20%20%20_createdAt%2C%0A%20%20%20%20%20%20summary%2C%0A%20%20%20%20%20%20title%2C%0A%20%20%20%20%20%20%22slug%22%3A%20slug.current%2C%0A%20%20%20%20%20%20%22image%22%3A%20image.asset%20-%3E%20url%2C%0A%20%20%20%20%20%20content%0A%7D"
+  )
+  return response.data.result
 }
 
 export async function getFeaturedBlogs(): Promise<Blog> {
@@ -33,25 +40,33 @@ export async function getFeaturedBlogs(): Promise<Blog> {
         "image": image.asset -> url,
         content
     }`
-  );
+  )
 }
 
 export async function getFaqs(): Promise<Faq[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "faq" ]| order(_createdAt asc) {_id,_createdAt,question,answer}`
-  );
+  )
 }
 
 export async function getBlogHero(): Promise<BlogHero[]> {
   return createClient(clientConfig).fetch(
-    groq`*[_type == "blogHero" ] {_id,_createdAt,"image": image.asset -> url,header,blogDesc,title}`
-  );
+    groq`*[_type == "blog"]{
+      _id,
+      _createdAt,
+      summary,
+      title,
+      "slug": slug.current,
+      "image": image.asset -> url,
+      content
+}`
+  )
 }
 
 export async function getReservations(): Promise<Reservations[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "reservations" ] {_id,_createdAt,"banner": banner.asset -> url,desc,callToAction,openingHours,title}`
-  );
+  )
 }
 
 export async function getCareerContent(): Promise<Careers[]> {
@@ -95,19 +110,19 @@ export async function getCareerContent(): Promise<Careers[]> {
   
     }
   }
-}`);
+}`)
 }
 
 export async function getOpenings(): Promise<Careers[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "openings" ] | order(_createdAt asc) `
-  );
+  )
 }
 
 export async function getValues(): Promise<Careers[]> {
   return createClient(clientConfig).fetch(
     groq`*[_type == "values" ]{_id,_createdAt,"image": image.asset -> url,valueDesc,value} `
-  );
+  )
 }
 
 export async function getHomeContent(): Promise<Home[]> {
@@ -134,14 +149,14 @@ export async function getHomeContent(): Promise<Home[]> {
     "bannerURL": banner.asset -> url
   }
 }`
-  );
+  )
 }
 
 export async function getCarousel(): Promise<Home[]> {
   return createClient(clientConfig)
     .fetch(groq`*[_type == "homeCarousel" ]{carousel[] {
       "imageURL": asset -> url
-    }} `);
+    }} `)
 }
 
 export async function getCardSection(): Promise<Home[]> {
@@ -151,7 +166,7 @@ export async function getCardSection(): Promise<Home[]> {
       buttonText,
       routes
     },}`
-  );
+  )
 }
 
 export async function getHomeNavigation(): Promise<Home[]> {
@@ -166,7 +181,7 @@ description,
       routes
     },
    
-} `);
+} `)
 }
 
 // Menu
@@ -184,7 +199,7 @@ export async function getMenuSectionOne(): Promise<Menu[]> {
     routes
   },
   "images": Images[].asset->url
-}`);
+}`)
 }
 
 export async function getMenus(): Promise<Menu[]> {
@@ -201,7 +216,7 @@ export async function getMenus(): Promise<Menu[]> {
     routes
   },
   "images": Images[].asset->url
-}`);
+}`)
 }
 
 export async function getSpecialMenu(): Promise<Menu[]> {
@@ -218,7 +233,7 @@ export async function getSpecialMenu(): Promise<Menu[]> {
     routes
   },
   "image": specialImage.asset->url
-}`);
+}`)
 }
 
 export async function getEvents(): Promise<Events[]> {
@@ -256,5 +271,5 @@ sectionThree{
     routes
   }
 }
-}`);
+}`)
 }
