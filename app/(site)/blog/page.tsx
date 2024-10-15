136a5b8bbddf57fd5fcee30dd2c099353a81eddb
Kelvin Tan <tnchnkit@hotmail.com>
2024-10-15 15:51:10 +0800

    fixed blog list bug

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,105 +1,109 @@
 "use client"
 import React, { useEffect, useState } from "react"
-import { blogPosts } from "../../data"
-import Card from "@/components/Card"
 import axios from "axios"
-import { useRouter } from "next/router"
-
-// type Props = {
-//   blogPosts:Array<{
-//     title: string;
-//     summary: string;
-//     image_url: string;
-//   }>;
-// }
-
+import Card from "@/components/Card"
+import { Spin } from "antd"
+import { RightOutlined, LeftOutlined } from "@ant-design/icons"
 export default function BlogPosts() {
-  // const router = useRouter()
   const [currentPage, setCurrentPage] = useState(1)
   const [blogPosts, setBlogPosts] = useState([])
-  const maxPostsperPage = 5
+  const [loading, setLoading] = useState(true)
+  const maxPostsPerPage = 5
 
-  const totalPages = Math.ceil(blogPosts.length / maxPostsperPage)
-
-  //To get current posts for the current page
-  const indexOfLastPost = currentPage * maxPostsperPage
-  const indexOfFirstPost = indexOfLastPost - maxPostsperPage
-  const currentPosts = blogPosts.slice(indexOfFirstPost, indexOfLastPost)
-
-  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
-
-  const fetchPosts = async () => {
-    const res = await axios.get(
-      "https://say9s8oc.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22blog%22%5D%7B%0A%20%20title%2C%0A%20%20%20%20slug%2C%0A%20%20summary%0A%7D"
-    )
-
-    setBlogPosts(res.data.result)
+  // Fetch blog posts
+  const fetchBlogs = async () => {
+    try {
+      const res = await axios.get(
+        "https://say9s8oc.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22blog%22%5D%20%7C%20order(_updatedAt%20desc)%20%7B%0A%20%20title%2C%0A%20%20%22slug%22%3A%20slug.current%2C%0A%20%20tags%2C%0A%20%20%22image%22%3A%20%7B%0A%20%20%20%20%22url%22%3A%20image.asset-%3Eurl%2C%0A%20%20%20%20%22alt%22%3A%20image.alt%0A%20%20%7D%2C%0A%20%20summary%5B%5D%20%7B%0A%20%20%20%20...%20%2F%2F%20returns%20all%20the%20blocks%20inside%20the%20summary%20array%0A%20%20%7D%2C%0A%20%20content%5B%5D%20%7B%0A%20%20%20%20...%2C%0A%20%20%20%20_type%20%3D%3D%20%22image%22%20%3D%3E%20%7B%0A%20%20%20%20%20%20%22url%22%3A%20asset-%3Eurl%2C%0A%20%20%20%20%20%20%22alt%22%3A%20alt%0A%20%20%20%20%7D%0A%20%20%7D%0A%7D%0A"
+      )
+      setBlogPosts(res.data.result)
+      setLoading(false)
+    } catch (error) {
+      console.error("Failed to fetch blog posts", error)
+    }
   }
 
+  // Call fetchBlogs when component mounts
   useEffect(() => {
-    fetchPosts()
+    fetchBlogs()
   }, [])
-  
-  //To handle previous page
+
+  // Calculate total pages
+  const totalPages = Math.ceil(blogPosts.length / maxPostsPerPage)
+
+  // Get current posts for the current page
+  const indexOfLastPost = currentPage * maxPostsPerPage
+  const indexOfFirstPost = indexOfLastPost - maxPostsPerPage
+  const currentPosts = blogPosts.slice(indexOfFirstPost, indexOfLastPost)
+
+  // Pagination functions
+  const paginate = (pageNumber: number) => setCurrentPage(pageNumber)
+
   const goToPreviousPage = () => {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1)
     }
   }
 
-  //To handle next page
   const goToNextPage = () => {
     if (currentPage < totalPages) {
       setCurrentPage(currentPage + 1)
     }
   }
 
+  // Render loading spinner while fetching data
+  if (loading) {
+    return <Spin spinning={loading} fullscreen />
+  }
+
   return (
     <div className="flex flex-col items-center py-16">
-      <h2 className = "font-bold mb-16">Featured Blogs</h2>
-      {blogPosts.map((post, index) => (
-        <div key={index} className="mb-7">
+      <h2 className="font-bold mb-16">Featured Blogs</h2>
+      {currentPosts.map((post, index) => (
+        <div key={index} className="mb-7 w-screen flex justify-center">
           <Card data={post} />
         </div>
       ))}
 
-      <div className="flex items-center space-x-2 mt-5">
-        <button
-          onClick={goToPreviousPage}
-          disabled={currentPage === 1}
-          className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 ${
-            currentPage === 1 ? "cursor-not-allowed opacity-50" : ""
-          }`}
-        >
-          &lt;
-        </button>
-
-        {Array.from({ length: totalPages }, (_, index) => index + 1).map((number) => (
+      {blogPosts.length > 1 && ( // Conditional rendering for pagination
+        <div className="flex items-center space-x-2 mt-5">
           <button
-            key={number}
-            onClick={() => paginate(number)}
-            className={`px-4 py-2 text-black border rounded ${
-              currentPage === number ? "bg-blue-300 border-blue-500" : ""
+            onClick={goToPreviousPage}
+            disabled={currentPage === 1}
+            className={`px-4 py-2 text-primary border-none rounded hover:text-primary/75 ${
+              currentPage === 1 ? "cursor-not-allowed opacity-50" : ""
             }`}
           >
-            {number}
+            <LeftOutlined />
           </button>
-        ))}
 
-        <button
-          onClick={goToNextPage}
-          disabled={currentPage === totalPages}
-          className={`px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 ${
-            currentPage === totalPages ? "cursor-not-allowed opacity-50" : ""
-          }`}
-        >
-          &gt;
-        </button>
-      </div>
+          {Array.from({ length: totalPages }, (_, index) => index + 1).map(
+            (number) => (
+              <button
+                key={number}
+                onClick={() => paginate(number)}
+                className={`px-4 py-2 text-black  ${
+                  currentPage === number
+                    ? "bg-white border-primary  border rounded"
+                    : ""
+                }`}
+              >
+                {number}
+              </button>
+            )
+          )}
 
-
-      
-
+          <button
+            onClick={goToNextPage}
+            disabled={currentPage === totalPages}
+            className={`px-4 py-2 text-primary border-none rounded hover:text-primary/75 ${
+              currentPage === totalPages ? "cursor-not-allowed opacity-50" : ""
+            }`}
+          >
+            <RightOutlined />
+          </button>
+        </div>
+      )}
     </div>
   )
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -2,19 +2,14 @@ import React from "react"
 import { PortableText } from "@portabletext/react"
 import urlBuilder from "@sanity/image-url"
 import { useRouter } from "next/navigation"
+import { IBlogPost } from "@/types/Blogs"
 // Define the types for the Card props
 interface CardProps {
-  data: {
-    title: string
-    slug: string
-    summary: string
-    image_url: string
-    tag: string
-  },
+  data: IBlogPost
   // router: any
 }
 
-const Card: React.FC<CardProps> = ({ data}) => {
+const Card: React.FC<CardProps> = ({ data }) => {
   const router = useRouter()
   console.log(data)
 
@@ -39,31 +34,32 @@ const Card: React.FC<CardProps> = ({ data}) => {
   }
 
   const handleBlogRedirect = () => {
-    router.push(`/blog/${data.slug.current}`)
-    console.log("hi")
+    router.push(`/blog/${data.slug}`)
   }
 
   return (
     <div
-      className="flex flex-row h-[303px] w-[1028px] border border-white bg-white py-4 px-4 rounded-lg"
+      className="flex flex-col md:flex-row h-auto md:h-[300px] mx-4 md:mx-0 md:w-4/5 max-w-[1028px] border border-white bg-white py-4 px-4 rounded-lg"
       style={{ boxShadow: "0 4px 6px rgba(0, 0, 0, 0.25)" }}
     >
       <img
-        className="w-1/3 h-auto rounded-lg object-cover"
-        src={data.image_url}
+        className="w-full md:w-1/3 h-auto rounded-lg object-cover min-h-[200px]"
+        src={data.image.url}
         alt={data.title}
       />
-      <div className="w-2/3 pl-7 mt-6">
-        <div className="flex flex-row gap-7 text-[#038885] mb-6">
-          <p>Innovation </p>
-          <p>Project Management</p>
+      <div className="md:w-2/3 md:pl-7 mt-6">
+        <div className="flex flex-row gap-7 text-[#038885] mb-4">
+          {data.tags && data.tags.map((tag: any) => <p>{tag}</p>)}
         </div>
-        <div className="font-bold text-[#01302F] text-2xl mb-6" onClick={handleBlogRedirect}>
+        <div
+          className="font-bold text-[#01302F] text-2xl mb-4 cursor-pointer"
+          onClick={handleBlogRedirect}
+        >
           {data.title}
         </div>
-        <p className="text-[#383838]">
+        <p className="text-[#383838] truncate-text-6">
           <PortableText
-            value={data?.summary}
+            value={data?.content}
             onMissingComponent={false}
             components={serializer}
           />
